048bbce8203244ba583f9219c2bd3fe94600fa02
jeonyul <jeonyul00@gmail.com>
2024-11-19 13:48:56 +0900

    first commit

---
+++ EverlandMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EverlandMap
+//
+//  Created by 전율 on 11/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
